72196947bc77cff114df78cdaffe69fe784689d3
jp <sainijaproz@gmail.com>
2026-08-30 23:19:45 -0400

    convert heavy img to avif

--- src/projects/ftr.tsx
+++ src/projects/ftr.tsx
@@ -12,25 +12,25 @@ export const FTRProject: React.FC<FTRProjectProps> = ({ project }) => {
 			<ProjectIntro project={project} />
 			<img
 				className="border border-gray-900 box-shadow my-2 w-full"
-				src="/projects/ftr/one.png"
+				src="/projects/ftr/one.avif"
 			/>
 			<div className="flex items-center gap-2">
 				<img
 					className="border border-gray-900 box-shadow w-1/2"
-					src="/projects/ftr/two.png"
+					src="/projects/ftr/two.avif"
 				/>
 				<img
 					className="border border-gray-900 box-shadow w-1/2"
-					src="/projects/ftr/three.png"
+					src="/projects/ftr/three.avif"
 				/>
 			</div>
 			<img
 				className="border border-gray-900 box-shadow my-2 w-full"
-				src="/projects/ftr/five.png"
+				src="/projects/ftr/five.avif"
 			/>
 			<img
 				className="border border-gray-900 box-shadow my-2 w-full"
-				src="/projects/ftr/four.png"
+				src="/projects/ftr/four.avif"
 			/>
 		</div>
 	);
