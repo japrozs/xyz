e78c9df180f13e3625694eeafa69ad3f1d13ec42
jp <sainijaproz@gmail.com>
2026-09-08 22:06:24 -0400

    add a compressed vid

--- src/projects/ftr.tsx
+++ src/projects/ftr.tsx
@@ -1,6 +1,6 @@
 import { ProjectIntro } from "@/components/project-intro";
 import { Project } from "@/types";
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useRef } from "react";
 
 interface FTRProjectProps {
 	project: Project;
@@ -11,6 +11,18 @@ export const FTRProject: React.FC<FTRProjectProps> = ({
 	project,
 	setSelected,
 }) => {
+	const videoRefs = useRef<(HTMLVideoElement | null)[]>([]);
+
+	useEffect(() => {
+		videoRefs.current.forEach((video) => {
+			if (video) {
+				video.muted = true;
+				video.play().catch(() => {
+					// autoplay was blocked; fails silently
+				});
+			}
+		});
+	}, []);
 	return (
 		<div>
 			<ProjectIntro project={project} setSelected={setSelected} />
@@ -32,6 +44,41 @@ export const FTRProject: React.FC<FTRProjectProps> = ({
 				className="border border-gray-900 box-shadow my-2 w-full"
 				src="/projects/ftr/five.avif"
 			/>
+			<div className="flex items-center mb-2 gap-2">
+				<video
+					ref={(el) => {
+						videoRefs.current[0] = el;
+					}}
+					className="border border-gray-900 shadow-sm w-1/3"
+					src="/projects/primary-union/six_compressed.mp4"
+					autoPlay
+					loop
+					muted
+					playsInline
+				/>
+				<video
+					ref={(el) => {
+						videoRefs.current[1] = el;
+					}}
+					className="border border-gray-900 shadow-sm w-1/3"
+					src="/projects/primary-union/seven_compressed.mp4"
+					autoPlay
+					loop
+					muted
+					playsInline
+				/>
+				<video
+					ref={(el) => {
+						videoRefs.current[2] = el;
+					}}
+					className="border border-gray-900 shadow-sm w-1/3"
+					src="/projects/primary-union/eight_compressed.mp4"
+					autoPlay
+					loop
+					muted
+					playsInline
+				/>
+			</div>
 			<img
 				className="border border-gray-900 box-shadow my-2 w-full"
 				src="/projects/ftr/four.avif"
